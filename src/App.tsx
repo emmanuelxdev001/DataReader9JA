ba1023dd7c0615567daa4ae2adb02bf89247e309
Emmanuel John <emmanueljohndev247@gmail.com>
2024-11-23 11:48:50 +0100

    awesome visualization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+
+import { mainRouter } from "./router/router";
+
+const App = () => {
+  return (
+    <div className="">
+      <RouterProvider router={mainRouter} />
+    </div>
+  );
+};
+
+export default App;
